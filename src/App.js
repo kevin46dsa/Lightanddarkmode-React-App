5dfd3223f9d0b175ca190ce3f095b5bd2ba71788
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-08-21 20:38:14 -0400

    light and dark mode done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {createContext, useState} from "react"
+import ReactSwitch from "react-switch"
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () =>{
+    setTheme((curr) => (curr === "light"? "dark" : "light"))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value = {{theme, toggleTheme}}>
+      <div className="App" id={theme}>
+        <h1>Light and Dark Mode implementation</h1>
+        <h2>Using this toggle button you can toggle between light and dark mode</h2>
+        <div className='switch'>
+          <label>{theme === 'light'? "Light Mode": "Dark Mode"}</label>
+        <ReactSwitch onChange={toggleTheme} checked = {theme === "dark"} />
+          </div>
+        </div>
+    </ThemeContext.Provider>
+    
   );
 }
 
